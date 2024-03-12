8857de84824d5fff2af25df5c5814dfafa6b8a61
Indrek Kalvet <ikalvet@uw.edu>
2024-03-12 16:20:55 -0700

    updated LigandMPNN submodule

--- scripts/ligandMPNN/helper_scripts/parse_multiple_chains.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --mem=32g
-#SBATCH -c 2
-#SBATCH --output=parse_multiple_chains.out
-
-source activate mlfold
-python parse_multiple_chains.py --input_path='/projects/ml/struc2seq/data_for_complexes/training_scripts/2022/LigandMPNN/benchmarking/clean_pdb_only' --output_path='./parsed_pdbs.jsonl'
